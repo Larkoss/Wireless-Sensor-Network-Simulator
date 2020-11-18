7abada93dadd0a2aecda075c4273984a6e98afee
Larkoss <klarko01@ucy.ac.cy>
2020-11-18 15:07:09 +0200

    Test commit

---
+++ homework3/src/cy/ac/ucy/cs/epl231/IDs10111821039279/homework3/HelloWorld.java
@@ -0,0 +1,7 @@
+package cy.ac.ucy.cs.epl231.IDs10111821039279.homework3;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("This will be printed\n");
+    }
+}
